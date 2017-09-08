github-af/SmartPGP

commit f2cd6ff8a49b7b5f9e2fe912807ff0b8a81d6b10

Single retrieval of RSA_PKCS1 cipher instance

src/fr/anssi/smartpgp/PGPKey.java
@@ -41,15 +41,18 @@ public final class PGPKey {
 
     private KeyPair keys;
 
+    private final Cipher cipher_rsa_pkcs1;
 
     protected PGPKey(final boolean for_secure_messaging) {
 
         is_secure_messaging_key = for_secure_messaging;
 
         if(is_secure_messaging_key) {
+            cipher = null;
             fingerprint = null;
             generation_date = null;
         } else {
+            cipher_rsa_pkcs1 = Cipher.getInstance(Cipher.ALG_RSA_PKCS1, false);
             fingerprint = new Fingerprint();
             generation_date = new byte[Constants.GENERATION_DATE_SIZE];
         }
@@ -624,11 +627,10 @@ public final class PGPKey {
                 return 0;
             }
 
-            final Cipher cipher = Cipher.getInstance(Cipher.ALG_RSA_PKCS1, false);
-            cipher.init(priv, Cipher.MODE_ENCRYPT);
+            cipher_rsa_pkcs1.init(priv, Cipher.MODE_ENCRYPT);
 
-            off = cipher.doFinal(buf, (short)0, lc,
-                                 buf, (short)lc);
+            off = cipher_rsa_pkcs1.doFinal(buf, (short)0, lc,
+                                           buf, (short)lc);
 
             return Util.arrayCopyNonAtomic(buf, (short)lc,
                                            buf, (short)0,
@@ -747,11 +749,10 @@ public final class PGPKey {
                 return 0;
             }
 
-            final Cipher cipher = Cipher.getInstance(Cipher.ALG_RSA_PKCS1, false);
-            cipher.init(priv, Cipher.MODE_DECRYPT);
+            cipher_rsa_pkcs1.init(priv, Cipher.MODE_DECRYPT);
 
-            final short len = cipher.doFinal(buf, (short)1, (short)(lc - 1),
-                                             buf, (short)lc);
+            final short len = cipher_rsa_pkcs1.doFinal(buf, (short)1, (short)(lc - 1),
+                                                       buf, (short)lc);
 
             off = Util.arrayCopyNonAtomic(buf, lc,
                                           buf, (short)0,

commit ee43681fdfaa293890fa88a6b37ae8466eb5d780

Fix problem introduced in commit f2cd6ff8a49b7b5f9e2fe912807ff0b8a81d6b10

src/fr/anssi/smartpgp/PGPKey.java
@@ -48,7 +48,7 @@ public final class PGPKey {
         is_secure_messaging_key = for_secure_messaging;
 
         if(is_secure_messaging_key) {
-            cipher = null;
+            cipher_rsa_pkcs1 = null;
             fingerprint = null;
             generation_date = null;
         } else {