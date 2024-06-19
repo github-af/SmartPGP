d2541526121bbb10f4676cf054c5479802841916
Phuong Lu <luhoaiphuong@gmail.com>
2024-06-19 19:49:33 +0700

    chore: support RSA4096 and upload CAP file

--- src/fr/anssi/smartpgp/Constants.java
+++ src/fr/anssi/smartpgp/Constants.java
@@ -25,7 +25,7 @@ import javacard.framework.*;
 public final class Constants {
 
     protected static final short INTERNAL_BUFFER_MAX_LENGTH =
-        (short)0x500;
+        (short)0x730; // to deal with RSA 4096 bits
 
     protected static final short APDU_MAX_LENGTH = (short)0x400;
 
